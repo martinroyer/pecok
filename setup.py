78a6419c3627e1c2b6c4d1c38b12f4f607fc1953
Martin Royer <martin.royer@m4x.org>
2017-10-17 11:42:06 +0200

    package structure

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name = 'pecok',
+    packages = ['pecok'],
+    version = '1.0.0',
+    description = 'Implementation of Pecok',
+    author='Martin Royer',
+    author_email='martin.royer@math.u-psud.fr',
+	url="https://github.com/martinroyer/pecok",
+	classifiers=[
+		  "Development Status :: 4 - Beta",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Topic :: Scientific/Engineering",
+          ]
+)
