00335fa2062261db533148424beefc6473792fcf
Martin Royer <martin.royer@m4x.org>
2017-10-18 17:33:02 +0200

    pecok method

---
+++ pecok/pecok_clustering.py
@@ -0,0 +1,17 @@
+"""Gamma estimation"""
+
+# author: Martin Royer <martin.royer@math.u-psud.fr>
+# License: MIT
+
+from .gamma import Gamma_hat4
+from .admm import pecok_admm
+
+###############################################################################
+def cluster(X, K):
+    """Implementation of PECOK estimator of B*
+
+    Parameters
+    ----------
+    X : array-like or sparse matrix, shape=(n_samples, n_features)
+        Training instances to cluster."""
+    return pecok_admm(X.dot(X.T)-Gamma_hat4(X), K)
