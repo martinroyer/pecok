cf8810b1d417fdcd9719fff35f58416884e6bd68
Martin Royer <martin.royer@m4x.org>
2019-01-27 16:21:53 +0100

    some tests

---
+++ tests/demo.py
@@ -0,0 +1,14 @@
+import numpy as np
+np.seed(232123123)
+n = 10
+p = 100
+X = np.zeros((n, p))
+snr = 0.3
+X[n//2:, :] = -np.ones(p) * snr + np.random.normal(scale=.1, size=(n//2,p))
+X[:n//2, :] = np.ones(p) * snr + np.random.normal(scale=1, size=(n//2,p))
+
+from sklearn.cluster import KMeans
+KMeans(n_clusters=2, init='k-means++', n_init=100).fit(X).labels_
+from pecok import PeCoK
+PeCoK(n_clusters=2, corr=0).fit(X).labels_
+PeCoK(n_clusters=2, corr=4).fit(X).labels_
